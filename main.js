e80af9d22bc3bf0f45583957dfa0bcbd339be645
akiliwebhost <web.services.edu@akiliuniverse.org>
2023-07-23 11:23:18 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+const blocks = document.querySelectorAll(".block");
+const resetDuration = 300;
+
+function startLoader() {
+	let counterElement = document.querySelector(".counter");
+	let currentValue = 0;
+
+	function updateCounter() {
+		if (currentValue === 100) {
+			return;
+		}
+
+		currentValue += Math.floor(Math.random() * 10) + 1;
+
+		if (currentValue > 100) {
+			currentValue = 100;
+		}
+
+		counterElement.textContent = currentValue
+
+		let delay = Math.floor(Math.random() * 200) + 50;
+		setTimeout(updateCounter, delay);
+	}
+
+	updateCounter();
+}
+
+startLoader();
+
+gsap.to(".counter", 0.25, {
+	delay: 3.5,
+	opacity: 0,
+});
+
+gsap.to(".bar", 1.5, {
+	delay: 3.5,
+	height: 0,
+	stagger: {
+		amount: 0.5,
+	},
+	ease: "power4.inOut",
+});
+
+gsap.from(".h1", 3, {
+	delay: 4,
+	y: 700,
+	stagger: {
+		amount: 0.5,
+	},
+	ease: "power4.inOut",
+});
+
+gsap.from(".h2", 1.5, {
+	delay: 4,
+	y: 700,
+	stagger: {
+		amount: 1.5,
+	},
+	ease: "power4.inOut"
+})
+
+gsap.from(".hero", 2, {
+	 delay: 6.5,
+	 y: 700,
+	 ease: "power4.inOut",
+});
+
+blocks.forEach((block) => {
+	let timeoutId;
+
+	block.addEventListener("mouseover", () => {
+		clearTimeout(timeoutId);
+		block.classList.add("active");
+		timoutId = setTimeout(() => {
+			block.classList.remove("active");
+		}, resetDuration);
+	});
+});
